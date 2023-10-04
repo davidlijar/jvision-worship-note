2131fc5e96d9d0854b6559cd367c8455b0e99228
davidlijar <davidbeljar@gmail.com>
2023-10-04 22:45:05 +0900

    ad

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jvision-worship-note',
+  base: '/jvision-worship-note/',
   plugins: [react()],
 })
